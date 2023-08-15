75c879a4d36f4c96705f32ae7cfdfd3c1dc1f19b
ageyev <ageyev@gmail.com>
2023-08-15 07:51:20 +0300

    prepare to publish on GitHUb/GitLab Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    open: true
   },
+  base: "./",
+  root: "./root",
   build: {
-    outDir: "build",
-    sourcemap: true,
+    assetsDir: "assets", // default
+    outDir: "../build",  //
+    sourcemap: true
   },
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "src/setupTests",
-    mockReset: true,
-  },
-})
+    mockReset: true
+  }
+});
