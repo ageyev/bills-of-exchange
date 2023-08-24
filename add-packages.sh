efcceda17830e3d202d5407e4ea8bf9ff74c19d9
ageyev <ageyev@gmail.com>
2023-08-24 07:39:54 +0300

    update

--- add-packages.sh
+++ add-packages.sh
@@ -6,5 +6,9 @@ npm install --save-dev @openzeppelin/contracts
 # Foundry
 # https://book.getfoundry.sh/getting-started/installation
 curl -L https://foundry.paradigm.xyz | bash
+source "${HOME}"/.bashrc
+foundryup
+git add . && git commit -m "update" # needed before forge init
+
 # TODO: See the foundry-rs/foundry-toolchain GitHub Action: https://github.com/foundry-rs/foundry-toolchain
 
