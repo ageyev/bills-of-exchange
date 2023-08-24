a33649bfb01cb1712f3bb24a899fc2fdd19344f7
ageyev <ageyev@gmail.com>
2023-08-24 06:15:55 +0300

    modify vite settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitest/config";
+import { loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
@@ -6,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
-    open: true
+    open: false
   },
   base: "./",
   root: "./src",
