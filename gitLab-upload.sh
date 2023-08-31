86da48ca9b02364f4e2d810c71f2c2f177b062a7
ageyev <ageyev@gmail.com>
2023-08-31 16:08:55 +0300

    build and upload to GitHub/GitLab web hosting (pages)

--- gitLab-upload.sh
+++
@@ -1,3 +0,0 @@
-
-npm run-script build
-git add . && git commit -a -m 'build and upload to GitLab web hosting (pages)' && git push gitlab --all && git push github --all
---
+++ publish.sh
@@ -0,0 +1,4 @@
+
+npm run-script build
+
+git add . && git commit -a -m 'build and upload to GitHub/GitLab web hosting (pages)' && git push gitlab --all && git push github --all
